a9a02bdb017c84280f3e610096d66e5dfe777a00
gopelkujo <sigantengnoval@gmail.com>
2025-11-11 00:24:46 +0700

    feat(footer): modify text

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
-    return (
-        <div className="w-screen flex justify-center p-10">
-            <span className="text-gray-500 font-extralight">Technical Test Anime Search App. Nauval 2025.</span>
-        </div>
-    );
+  return (
+    <div className="w-screen flex justify-center p-10">
+      <span className="text-gray-500 font-extralight">
+        Anime Search App, plain React JS + Tailwind + Shadcn UI + Redux. Nauval
+        2025.
+      </span>
+    </div>
+  );
 }
