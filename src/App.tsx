d214f6e12e8ca162d0075af09ad03789f10c469b
gopelkujo <sigantengnoval@gmail.com>
2025-11-11 09:49:20 +0700

    chore: remove unused file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Counter from "./features/counter/Counter";
 import Layout from "./components/sections/layout";
 import Home from "./features/home/Home";
 import NotFound from "./NotFound";
@@ -13,7 +12,6 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/detail/:id" element={<Detail />} />
-          <Route path="/counter" element={<Counter />} />
         </Route>
 
         <Route path="*" element={<NotFound />} />
